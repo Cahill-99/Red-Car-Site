4d60933c3e46003eec8442a07eca67493016d452
Cahill-99 <ecahill001@gmail.com>
2021-04-05 13:49:44 -0230

    js changes

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+let scrollingButton = document.getElementById("scrollBtn");
+scrollingButton.addEventListener("click", function(){
+    window.scrollTo(0, 623);
+});
+                            
                
-               
-into.addEventListener('click', (ev)=>{
+/*into.addEventListener('click', (ev)=>{
     by.scrollIntoView(true);
+}*/
